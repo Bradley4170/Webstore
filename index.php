70e65ba6bbd128c86654e3548ae0222de6943e30
Bradley Gillies <bradleygillies4170@gmail.com>
2018-10-14 17:26:43 +0100

    Responsive

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 <title>Webstore</title>
 <link rel="stylesheet" href="css/main.css" type="text/css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <head>
     <?php require 'php/menu.php' ?>
 </head>
