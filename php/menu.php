46173b80ea1660df93d7ab691a75a41fc5eda842
Nick-Werkman <nickwerkman7@gmail.com>
2018-10-14 18:28:37 +0200

    menu class

--- php/menu.php
+++ php/menu.php
@@ -1,8 +1,8 @@
 <nav>
     <ul>
-        <li><a href="#"></a>Home</li>
-        <li><a href="#"></a>Page 1</li>
-        <li><a href="#"></a>Page 2</li>
-        <li><a href="#"></a>Page 3</li>
+        <li class="menu"><a href="#"></a>Home</li>
+        <li class="menu"><a href="#"></a>Page 1</li>
+        <li class="menu"><a href="#"></a>Page 2</li>
+        <li class="menu"><a href="#"></a>Page 3</li>
     </ul>
 </nav>
