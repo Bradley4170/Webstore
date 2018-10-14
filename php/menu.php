f5c76abab3d2572eb8b94ea51fd322da4d63d9b2
Nick-Werkman <nickwerkman7@gmail.com>
2018-10-14 18:04:27 +0200

    menu

---
+++ php/menu.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="#"></a>Home</li>
+        <li><a href="#"></a>Page 1</li>
+        <li><a href="#"></a>Page 2</li>
+        <li><a href="#"></a>Page 3</li>
+    </ul>
+</nav>
